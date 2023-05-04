40b862c4131643d8e35e1ffe67966bfdbf922b17
Ilya Lazykin <panda007a@mail.ru>
2023-05-04 22:36:13 +0300

    Added commentary, splitted big methods to smaller ones

--- Task/Task/ListSerializerService.cs
+++ Task/Task/ListSerializerService.cs
@@ -7,23 +7,57 @@ using System.Text.RegularExpressions;
 
 namespace Task
 {
+    /// <summary>
+    /// This class handles linked list serialization and deserialization.
+    /// </summary>
     public static class ListSerializerService
     {
         private static readonly int FS_END = -1;
         private static readonly string DATA_FIELD = "Data";
         private static readonly string RAND_FIELD = "Rand";
+        
+        /// <summary>
+        /// Serialize linked list and write to file stream.
+        /// </summary>
+        /// <param name="fs">File stream to write linked list</param>
+        /// <param name="listRand">Linked list to serialize</param>
         public static void Serialize(FileStream s, ListRand listRand)
         {
-            ListNode curNode = listRand.Head;
-            StringBuilder sb = new StringBuilder();
-            Dictionary<ListNode, int> nodeIndexes = new Dictionary<ListNode, int>();
+            var nodeIndexes = IndexingNodes(listRand);
 
+            var utf8 = new UTF8Encoding();
+            var result = utf8.GetBytes(WriteNodes(nodeIndexes));
+
+            s.Write(result, 0, result.Length);
+        }
+        
+        /// <summary>
+        /// Indexing nodes 
+        /// </summary>
+        /// <param name="listRand"> Linked list to take nodes from</param>
+        /// <returns> Dict with nodes linked with theirs index</returns>
+        private static Dictionary<ListNode, int> IndexingNodes(ListRand listRand)
+        {
+            ListNode curNode = listRand.Head;
+            Dictionary<ListNode, int> nodeIndexes = new Dictionary<ListNode, int>();
+            
             for (int i = 0; i < listRand.Count; i++)
             {
                 nodeIndexes.Add(curNode, i);
                 curNode = curNode.Next;
             }
 
+            return nodeIndexes;
+        }
+        
+        /// <summary>
+        /// Writes nodes data and rand field to Json format
+        /// </summary>
+        /// <param name="nodeIndexes"> dict of nodes paired with index</param>
+        /// <returns> Json string with nodes params</returns>
+        private static string WriteNodes(Dictionary<ListNode, int> nodeIndexes)
+        {
+            StringBuilder sb = new StringBuilder();
             sb.Append("[");
             foreach (var node in nodeIndexes)
             {
@@ -38,17 +72,19 @@ namespace Task
             }
             sb.Remove(sb.Length - 1, 1); // removing extra comma
             sb.Append("]");
-            
-            var utf8 = new UTF8Encoding();
-            var result = utf8.GetBytes(sb.ToString());
 
-            s.Write(result, 0, result.Length);
+            return sb.ToString();
         }
 
+        /// <summary>
+        /// Deserialize linked list from filestream.
+        /// </summary>
+        /// <param name="s">File stream with serialized linked list</param>
+        /// <param name="listRand">Linked list to serialize</param>
+        /// <returns> deserialized ListRand</returns>
         public static ListRand Deserialize(FileStream s)
         {
             char objectFirstByte = '{';
-            string rawObjectData;
             ListNode[] nodeArr;
             List<Dictionary<string, string>> nodesData = new List<Dictionary<string, string>>();
 
@@ -56,24 +92,11 @@ namespace Task
             {
                 if (curByte == objectFirstByte)
                 {
-                    nodesData.Add(ReadObject(s));
+                    nodesData.Add(ReadNode(s));
                 }
             }
 
-            nodeArr = new ListNode[nodesData.Count];
-
-            for (int i = 0; i < nodesData.Count; i++)
-            {
-                nodeArr[i] = new ListNode();
-            }
-
-            for (int i = 0; i < nodesData.Count; i++)
-            {
-                nodeArr[i].Data = nodesData[i][DATA_FIELD];
-                nodeArr[i].Next = nodeArr.ElementAtOrDefault(i + 1) ?? null;
-                nodeArr[i].Prev = nodeArr.ElementAtOrDefault(i - 1) ?? null;
-                nodeArr[i].Rand = nodeArr[Convert.ToInt32(nodesData[i][RAND_FIELD])];
-            }
+            nodeArr = CreateNodes(nodesData);
 
             var result = new ListRand()
             {
@@ -81,11 +104,16 @@ namespace Task
                 Head = nodeArr[0],
                 Tail = nodeArr[nodeArr.Length - 1]
             };
-
+            
             return result;
         }
 
-        private static Dictionary<string, string> ReadObject(FileStream s)
+        /// <summary>
+        /// Reads names and values of node params
+        /// </summary>
+        /// <param name="s">File stream with serialized linked list</param>
+        /// <returns> Dict with pairs of names and values of nodes params</returns>
+        private static Dictionary<string, string> ReadNode(FileStream s)
         {
             StringBuilder sb = new StringBuilder();
             bool isReadingKeyOrValue = false;
@@ -106,7 +134,6 @@ namespace Task
                         else
                         {
                             keyName = sb.ToString();
-
                         }
                         isReadingKey = !isReadingKey;
                         sb.Clear();
@@ -126,6 +153,31 @@ namespace Task
                 }
             }
             return objectParams;
+        }
+
+        /// <summary>
+        /// Creates array of nodes and fill them
+        /// </summary>
+        /// <param name="nodesData"> data to create nodes from</param>
+        /// <returns> array of filled linked nodes</returns>
+        private static ListNode[] CreateNodes(List<Dictionary<string, string>> nodesData)
+        {
+            ListNode[] nodeArr = new ListNode[nodesData.Count];
+
+            for (int i = 0; i < nodesData.Count; i++)
+            {
+                nodeArr[i] = new ListNode();
+            }
+
+            for (int i = 0; i < nodesData.Count; i++)
+            {
+                nodeArr[i].Data = nodesData[i][DATA_FIELD];
+                nodeArr[i].Next = nodeArr.ElementAtOrDefault(i + 1) ?? null;
+                nodeArr[i].Prev = nodeArr.ElementAtOrDefault(i - 1) ?? null;
+                nodeArr[i].Rand = nodeArr[Convert.ToInt32(nodesData[i][RAND_FIELD])];
+            }
+
+            return nodeArr;
         }
     }
 }
